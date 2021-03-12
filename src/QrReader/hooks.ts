a49fd93d00cf08d098ea4edccd829f2bb44121a9
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2021-03-11 22:23:24 -0300

    chore: minor changes

--- src/QrReader/hooks.ts
+++ src/QrReader/hooks.ts
@@ -3,7 +3,7 @@ import { BrowserQRCodeReader, IScannerControls } from '@zxing/browser';
 
 import { UseQrReaderHook } from '../types';
 
-import * as Helpers from './utils';
+import { isMediaDevicesSupported, isValidType, isValidValue } from './utils';
 
 // TODO: add support for debug logs
 export const useQrReader: UseQrReaderHook = ({
@@ -20,8 +20,8 @@ export const useQrReader: UseQrReaderHook = ({
     });
 
     if (
-      !Helpers.isMediaDevicesSupported() &&
-      Helpers.isValidType(onResult, 'onResult', 'function')
+      !isMediaDevicesSupported() &&
+      isValidType(onResult, 'onResult', 'function')
     ) {
       const message =
         'MediaDevices API has no support for your browser. You can fix this by running "npm i webrtc-adapter"';
@@ -30,8 +30,8 @@ export const useQrReader: UseQrReaderHook = ({
     }
 
     if (
-      Helpers.isValidType(facingMode, 'facingMode', 'string') &&
-      Helpers.isValidValue(facingMode, 'facingMode', [
+      isValidType(facingMode, 'facingMode', 'string') &&
+      isValidValue(facingMode, 'facingMode', [
         'user',
         'left',
         'right',
@@ -47,13 +47,13 @@ export const useQrReader: UseQrReaderHook = ({
 
       codeReader
         .decodeFromConstraints(constraints, videoId, (result, error) => {
-          if (Helpers.isValidType(onResult, 'onResult', 'function')) {
+          if (isValidType(onResult, 'onResult', 'function')) {
             onResult(result, error, codeReader);
           }
         })
         .then((controls: IScannerControls) => (controlsRef.current = controls))
         .catch((error: Error) => {
-          if (Helpers.isValidType(onResult, 'onResult', 'function')) {
+          if (isValidType(onResult, 'onResult', 'function')) {
             onResult(null, error, codeReader);
           }
         });
